fbd063c641c560c36e3a8d1e02dd820ee4f8f0e3
Zach Nicoll <zach.nicoll55@gmail.com>
2021-03-17 22:36:09 +1000

    chore: frame_queue documentation

--- src/frame_queue.cpp
+++ src/frame_queue.cpp
@@ -3,8 +3,10 @@
 // Add new frame to end of frame queue
 void frame_queue_push(VideoReader *video_reader, uint8_t *new_frame)
 {
+  // Don't add node if queue is already at max length
   if (video_reader->frame_queue_length < MAX_QUEUE_LENGTH)
   {
+    // Base case where frame_queue's initial frame_data will be NULL
     if (video_reader->frame_queue->frame_data == NULL)
     {
       video_reader->frame_queue->frame_data = new_frame;
@@ -13,16 +15,22 @@ void frame_queue_push(VideoReader *video_reader, uint8_t *new_frame)
     {
       FrameNode *node = video_reader->frame_queue;
 
+      // Find the end of the queue i.e. where a node's next_frame is NULL
       while (node->next_frame != NULL)
       {
         node = node->next_frame;
       }
 
+      // Set last node's frame_data to new frame data
       node->frame_data = new_frame;
+
+      // Set last node's next_frame to an empty FrameNode
       node->next_frame = new FrameNode;
       node->next_frame->frame_data = NULL;
       node->next_frame->next_frame = NULL;
     }
+
+    // Increase queue length so we know how many frames there are
     video_reader->frame_queue_length++;
   }
 }
@@ -32,7 +40,11 @@ void frame_queue_consume(VideoReader *video_reader, FrameNode **node_out)
 {
   if (video_reader->frame_queue_length > 0)
   {
+    // Assign first frame in the queue to external pointer
     *node_out = video_reader->frame_queue;
+
+    // Set first frame node pointer in the queue to the next frame node,
+    // effectively removing the head from the queue
     video_reader->frame_queue = video_reader->frame_queue->next_frame;
     video_reader->frame_queue_length--;
   }
@@ -45,6 +57,7 @@ void frame_queue_cleanup_node(FrameNode *node)
 
 void frame_queue_cleanup(FrameNode *node)
 {
+  // Recursively free all nodes in a frame queue
   if (node != NULL && node->frame_data != NULL) {
     frame_queue_cleanup_node(node);
     frame_queue_cleanup(node->next_frame);
